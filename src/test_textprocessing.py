5d4bfd8c5d655a2ade4a2909ad2ff248deb3c706
ApoloLoxias <apololoxias@gmail.com>
2025-12-27 17:49:22 -0300

    spliit_nodes_delimiter

--- src/test_textprocessing.py
+++ src/test_textprocessing.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textprocessing import text_node_to_html_node
+from textprocessing import text_node_to_html_node, split_nodes_delimiter
 from textnode import TextType, TextNode
 from htmlnode import LeafNode
 
@@ -36,3 +36,202 @@ class test_text_node_to_html_node(unittest.TestCase):
             self.assertEqual(i.value, o.value)
             self.assertEqual(i.children, o.children)
             self.assertEqual(i.props, o.props)
+
+test_splitables = [
+    TextNode("Some **bold text** inside some **plain text**", TextType.PLAIN_TEXT),
+    TextNode("Some _italic_ text _inside_ some _plain_ text", TextType.PLAIN_TEXT),
+    TextNode("Some `code` inside some `plain text`", TextType.PLAIN_TEXT),
+    TextNode("Some _italic text_ inside bold text", TextType.BOLD),
+    TextNode("Some `code` inside bold text", TextType.BOLD),
+    TextNode("Some **bold text** inside italic text", TextType.ITALIC),
+    TextNode("Some `code` inside italic text", TextType.ITALIC),
+    TextNode("Some **bold text** inside code", TextType.CODE),
+    TextNode("Some _italic text_ inside code", TextType.CODE),
+]
+test_not_splitables = [TextNode("Some **incorrectly formated markdown", TextType.PLAIN_TEXT)]
+test_splited_plain_bold = [
+    TextNode("Some ", TextType.PLAIN_TEXT),
+    TextNode("bold text", TextType.BOLD),
+    TextNode(" inside some ", TextType.PLAIN_TEXT),
+    TextNode("plain text", TextType.BOLD),
+    TextNode("Some _italic_ text _inside_ some _plain_ text", TextType.PLAIN_TEXT),
+    TextNode("Some `code` inside some `plain text`", TextType.PLAIN_TEXT),
+    TextNode("Some _italic text_ inside bold text", TextType.BOLD),
+    TextNode("Some `code` inside bold text", TextType.BOLD),
+    TextNode("Some **bold text** inside italic text", TextType.ITALIC),
+    TextNode("Some `code` inside italic text", TextType.ITALIC),
+    TextNode("Some **bold text** inside code", TextType.CODE),
+    TextNode("Some _italic text_ inside code", TextType.CODE),
+]
+test_splited_plain_italic = [
+    TextNode("Some **bold text** inside some **plain text**", TextType.PLAIN_TEXT),
+    TextNode("Some ", TextType.PLAIN_TEXT),
+    TextNode("italic", TextType.ITALIC),
+    TextNode(" text ", TextType.PLAIN_TEXT),
+    TextNode("inside", TextType.ITALIC),
+    TextNode(" some ", TextType.PLAIN_TEXT),
+    TextNode("plain", TextType.ITALIC),
+    TextNode(" text", TextType.PLAIN_TEXT),
+    TextNode("Some `code` inside some `plain text`", TextType.PLAIN_TEXT),
+    TextNode("Some _italic text_ inside bold text", TextType.BOLD),
+    TextNode("Some `code` inside bold text", TextType.BOLD),
+    TextNode("Some **bold text** inside italic text", TextType.ITALIC),
+    TextNode("Some `code` inside italic text", TextType.ITALIC),
+    TextNode("Some **bold text** inside code", TextType.CODE),
+    TextNode("Some _italic text_ inside code", TextType.CODE),
+]
+test_splited_plain_code = [
+    TextNode("Some **bold text** inside some **plain text**", TextType.PLAIN_TEXT),
+    TextNode("Some _italic_ text _inside_ some _plain_ text", TextType.PLAIN_TEXT),
+    TextNode("Some ", TextType.PLAIN_TEXT),
+    TextNode("code", TextType.CODE),
+    TextNode(" inside some ", TextType.PLAIN_TEXT),
+    TextNode("plain text", TextType.CODE),
+    TextNode("Some _italic text_ inside bold text", TextType.BOLD),
+    TextNode("Some `code` inside bold text", TextType.BOLD),
+    TextNode("Some **bold text** inside italic text", TextType.ITALIC),
+    TextNode("Some `code` inside italic text", TextType.ITALIC),
+    TextNode("Some **bold text** inside code", TextType.CODE),
+    TextNode("Some _italic text_ inside code", TextType.CODE)
+]
+test_splited_bold_italic = [
+    TextNode("Some **bold text** inside some **plain text**", TextType.PLAIN_TEXT),
+    TextNode("Some _italic_ text _inside_ some _plain_ text", TextType.PLAIN_TEXT),
+    TextNode("Some `code` inside some `plain text`", TextType.PLAIN_TEXT),
+    TextNode("Some ", TextType.BOLD),
+    TextNode("italic text", TextType.ITALIC),
+    TextNode(" inside bold text", TextType.BOLD),
+    TextNode("Some `code` inside bold text", TextType.BOLD),
+    TextNode("Some **bold text** inside italic text", TextType.ITALIC),
+    TextNode("Some `code` inside italic text", TextType.ITALIC),
+    TextNode("Some **bold text** inside code", TextType.CODE),
+    TextNode("Some _italic text_ inside code", TextType.CODE),
+]
+test_splited_bold_code = [
+    TextNode("Some **bold text** inside some **plain text**", TextType.PLAIN_TEXT),
+    TextNode("Some _italic_ text _inside_ some _plain_ text", TextType.PLAIN_TEXT),
+    TextNode("Some `code` inside some `plain text`", TextType.PLAIN_TEXT),
+    TextNode("Some _italic text_ inside bold text", TextType.BOLD),
+    TextNode("Some ", TextType.BOLD),
+    TextNode("code", TextType.CODE),
+    TextNode(" inside bold text", TextType.BOLD),
+    TextNode("Some **bold text** inside italic text", TextType.ITALIC),
+    TextNode("Some `code` inside italic text", TextType.ITALIC),
+    TextNode("Some **bold text** inside code", TextType.CODE),
+    TextNode("Some _italic text_ inside code", TextType.CODE),
+]
+test_splited_italic_bold = [
+    TextNode("Some **bold text** inside some **plain text**", TextType.PLAIN_TEXT),
+    TextNode("Some _italic_ text _inside_ some _plain_ text", TextType.PLAIN_TEXT),
+    TextNode("Some `code` inside some `plain text`", TextType.PLAIN_TEXT),
+    TextNode("Some _italic text_ inside bold text", TextType.BOLD),
+    TextNode("Some `code` inside bold text", TextType.BOLD),
+    TextNode("Some ", TextType.ITALIC),
+    TextNode("bold text", TextType.BOLD),
+    TextNode(" inside italic text", TextType.ITALIC),
+    TextNode("Some `code` inside italic text", TextType.ITALIC),
+    TextNode("Some **bold text** inside code", TextType.CODE),
+    TextNode("Some _italic text_ inside code", TextType.CODE),
+]
+test_splited_italic_code = [
+    TextNode("Some **bold text** inside some **plain text**", TextType.PLAIN_TEXT),
+    TextNode("Some _italic_ text _inside_ some _plain_ text", TextType.PLAIN_TEXT),
+    TextNode("Some `code` inside some `plain text`", TextType.PLAIN_TEXT),
+    TextNode("Some _italic text_ inside bold text", TextType.BOLD),
+    TextNode("Some `code` inside bold text", TextType.BOLD),
+    TextNode("Some **bold text** inside italic text", TextType.ITALIC),
+    TextNode("Some ", TextType.ITALIC),
+    TextNode("code", TextType.CODE),
+    TextNode(" inside italic text", TextType.ITALIC),
+    TextNode("Some **bold text** inside code", TextType.CODE),
+    TextNode("Some _italic text_ inside code", TextType.CODE),
+]
+test_splited_code_bold = [
+    TextNode("Some **bold text** inside some **plain text**", TextType.PLAIN_TEXT),
+    TextNode("Some _italic_ text _inside_ some _plain_ text", TextType.PLAIN_TEXT),
+    TextNode("Some `code` inside some `plain text`", TextType.PLAIN_TEXT),
+    TextNode("Some _italic text_ inside bold text", TextType.BOLD),
+    TextNode("Some `code` inside bold text", TextType.BOLD),
+    TextNode("Some **bold text** inside italic text", TextType.ITALIC),
+    TextNode("Some `code` inside italic text", TextType.ITALIC),
+    TextNode("Some ", TextType.CODE),
+    TextNode("bold text", TextType.BOLD),
+    TextNode(" inside code", TextType.CODE),
+    TextNode("Some _italic text_ inside code", TextType.CODE),
+]
+test_splited_code_italic = [
+    TextNode("Some **bold text** inside some **plain text**", TextType.PLAIN_TEXT),
+    TextNode("Some _italic_ text _inside_ some _plain_ text", TextType.PLAIN_TEXT),
+    TextNode("Some `code` inside some `plain text`", TextType.PLAIN_TEXT),
+    TextNode("Some _italic text_ inside bold text", TextType.BOLD),
+    TextNode("Some `code` inside bold text", TextType.BOLD),
+    TextNode("Some **bold text** inside italic text", TextType.ITALIC),
+    TextNode("Some `code` inside italic text", TextType.ITALIC),
+    TextNode("Some **bold text** inside code", TextType.CODE),
+    TextNode("Some ", TextType.CODE),
+    TextNode("italic text", TextType.ITALIC),
+    TextNode(" inside code", TextType.CODE),
+]
+
+class test_split_nodes_delimiter(unittest.TestCase):
+    def test_plain_text_bold(self):
+        self.assertEqual(
+            split_nodes_delimiter(test_splitables, "**", TextType.PLAIN_TEXT),
+            test_splited_plain_bold
+        )
+    def test_plain_text_italic(self):
+        self.assertEqual(
+            split_nodes_delimiter(test_splitables, "_", TextType.PLAIN_TEXT),
+            test_splited_plain_italic
+        )
+    def test_plain_text_code(self):
+        self.assertEqual(
+            split_nodes_delimiter(test_splitables, "`", TextType.PLAIN_TEXT),
+            test_splited_plain_code
+        )
+    def test_bold_italic(self):
+        self.assertEqual(
+            split_nodes_delimiter(test_splitables, "_", TextType.BOLD),
+            test_splited_bold_italic
+        )
+    def test_bold_code(self):
+        self.assertEqual(
+            split_nodes_delimiter(test_splitables, "`", TextType.BOLD),
+            test_splited_bold_code
+        )
+    def test_italic_bold(self):
+        self.assertEqual(
+            split_nodes_delimiter(test_splitables, "**", TextType.ITALIC),
+            test_splited_italic_bold
+        )
+    def test_italic_code(self):
+        self.assertEqual(
+            split_nodes_delimiter(test_splitables, "`", TextType.ITALIC),
+            test_splited_italic_code
+        )
+    def test_code_bold(self):
+        self.assertEqual(
+            split_nodes_delimiter(test_splitables, "**", TextType.CODE),
+            test_splited_code_bold
+        )
+    def test_code_italic(self):
+        self.assertEqual(
+            split_nodes_delimiter(test_splitables, "_", TextType.CODE),
+            test_splited_code_italic
+        )
+    def test_invalid_input(self):
+        self.assertRaises(
+            ValueError,
+            split_nodes_delimiter,
+            test_splitables, "**", TextType.IMAGE
+        )
+        self.assertRaises(
+        ValueError,
+        split_nodes_delimiter,
+        test_splitables, "*", TextType.PLAIN_TEXT
+        )
+        self.assertRaises(
+        Exception,
+        split_nodes_delimiter,
+        test_not_splitables, "**", TextType.PLAIN_TEXT
+        )
--- src/textprocessing.py
+++ src/textprocessing.py
@@ -20,3 +20,44 @@ def text_node_to_html_node(text_node: TextNode) -> LeafNode:
             return LeafNode(tag="a", value=text_node.text, props={"href":text_node.url})
         case TextType.IMAGE:
             return LeafNode(tag="img", value="", props={"src":text_node.url, "alt":text_node.text})
+
+
+
+def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+    match delimiter:
+        case "**":
+            inside_text_type = TextType.BOLD
+        case "_":
+            inside_text_type = TextType.ITALIC
+        case "`":
+            inside_text_type = TextType.CODE
+        case _:
+            raise ValueError("Delimiter not implemented")
+    if text_type not in [
+        TextType.PLAIN_TEXT,
+        TextType.BOLD,
+        TextType.ITALIC,
+        TextType.CODE,
+    ]:
+        raise ValueError(f"TextType {text_type} not implemented")
+
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != text_type:
+            new_nodes.append(node)
+            continue
+        else:
+            new_texts = node.text.split(delimiter)
+            if len(new_texts) % 2 == 0:
+                raise Exception("Invalid markdown syntax")
+            for i in range(0, len(new_texts)):
+                new_text = new_texts[i]
+                if new_text == "":
+                    continue
+                if i % 2 == 0:
+                    new_text_type = node.text_type
+                else:
+                    new_text_type = inside_text_type
+                new_node = TextNode(text=new_text, text_type=new_text_type)
+                new_nodes.append(new_node)
+    return new_nodes
